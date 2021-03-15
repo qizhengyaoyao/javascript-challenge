ea68ce28d3c46f9c7da9edf46add8e0edce8953e
qizhengyaoyao <28369996+qizhengyaoyao@users.noreply.github.com>
2021-03-15 15:24:24 +1100

    UFO-level-1 done.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,54 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// console.log(tableData);
+var tbody = d3.select("tbody");
+
+function addUFOdata(UFOdata) {
+  var row = tbody.append("tr");
+
+  Object.entries(UFOdata).forEach(function([key, value]) {
+    // console.log(key, value);
+    var cell = row.append("td");
+    cell.text(value);
+    });
+};
+
+tableData.forEach(addUFOdata);
+
+var button = d3.select("#filter-btn");
+
+button.on("click", runEnter);
+
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    // console.log(inputValue);
+  
+    var filteredData = tableData.filter(tableData => tableData.datetime == inputValue);
+  
+    // console.log(filteredData);
+
+    tbody.html("");
+
+    filteredData.forEach(addUFOdata);
+
+    if(filteredData.length !== 0) {
+        filteredData.forEach(addUFOdata);
+    }
+    else {
+        var cell = tbody.append("tr").append("td");
+        cell.text("No Data matches the input value.");
+        cell.attr("colspan", "7");
+    }
+    
+        
+  };
